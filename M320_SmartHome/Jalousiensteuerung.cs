3e527c06ec5bc561c14bc1bb7cbb800d5c243fb2
Ajnur Ademi <Ajnur.Ademi@ksb-sg.ch>
2024-02-26 08:33:27 +0100

    Logik für Jalousiesteuerung.cs (in der Klasse besprochen)

--- M320_SmartHome/Jalousiensteuerung.cs
+++ M320_SmartHome/Jalousiensteuerung.cs
@@ -1,22 +1,21 @@
 ﻿namespace M320_SmartHome {
     public class Jalousiensteuerung : ZimmerDecorator {
-
-        // Die EIgenschaft {JalousieOffen} sollte Anfangs auf "true" gesetzt werden,
-        // damit sie überhaupt in der if-Abfrage überprüft werden kann, sonst wäre die Eigenschaft noch nicht gesetzt (weder true noch false)
-        // --> siehe CodeZeile = 13 //if(JalousieOffen)//
-        public bool JalousieOffen { get; set; } = true;
+        
+        // Logik abgeändert (in der Klasse besprochen)
+        public bool JalousieOffen { get; set; }
         public Jalousiensteuerung(IZimmer zimmer) : base(zimmer) { }
 
         public override void VerarbeiteWetterdaten(Wetterdaten wetterdaten) {
             base.VerarbeiteWetterdaten(wetterdaten);
             if(wetterdaten.Aussentemperatur > TemperaturVorgabe) {
                 if(JalousieOffen) {
-                    if(PersonenImZimmer) {
-                        Console.WriteLine("Jalousie kann nicht geschlossen werden weil Personen im Zimmer sind.");
-                    } else {
-                        Console.WriteLine($"Jalousie wird geschlossen.");
-                        JalousieOffen = false;
-                    }
+                    Console.WriteLine($"Jalousie wird geschlossen.");
+                    JalousieOffen = false;
+                }
+                else if(PersonenImZimmer == true)
+                {
+                    Console.WriteLine("Jalousie kann nicht geschlossen werden weil Personen im Zimmer sind.");
+                    JalousieOffen = true;
                 }
             } else {
                 if(!JalousieOffen) {
